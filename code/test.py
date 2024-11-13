09115a563951e215f8d52e70ea967bbcd7e1a96f
Korvin Venzke <korvin.venzke@gmail.com>
2024-11-13 15:33:02 +0100

    create own python script

--- code/test.py
+++ code/test.py
@@ -1,11 +1,12 @@
-import geopandas as gpd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# generate 200 random 2D points between 0 and 10000 in the from: (i, x, y)
-points = np.random.randint(0, 10000, (200, 2))
+# generate 200 random data points with id
+data = np.random.randint(0, 10000, (200, 2))
+data = np.hstack((np.arange(1, 201).reshape(-1, 1), data))
 
-# plot the points
-plt.scatter(points[:, 0], points[:, 1], s=1)
+# plot the data
+plt.scatter(data[:, 1], data[:, 2])
 plt.show()
+
